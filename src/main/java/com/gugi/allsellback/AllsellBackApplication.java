84d7295629353ea7d98bfa3ee4937795cc2d11a4
Johnny Jeong <ksjeong@gmail.com>
2021-11-09 21:51:58 +1100

    first commit

---
+++ src/main/java/com/gugi/allsellback/AllsellBackApplication.java
@@ -0,0 +1,13 @@
+package com.gugi.allsellback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllsellBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllsellBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gugi/allsellback/AllsellBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gugi.allsellback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllsellBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
